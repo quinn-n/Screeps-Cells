5f3972c3759439a126ac803a892ddf1b7ce1af47
Quinn N <88954051+quinn-n@users.noreply.github.com>
2024-12-17 23:27:41 -0500

    Add `generateHarvesterBody` and supporting code

--- src/allocator.ts
+++ src/allocator.ts
@@ -44,6 +44,8 @@ class Allocator implements Ticker {
 	 * Pulls from unused creeps first, then spawns largest creeps possible.
 	 */
 	public addRequiredHarvesters(source: Source, minEnergy: number) {
+		const room = source.room as BaseRoom;
+
 		const currentEstimate = this.getEstimatedSourceExtractionPerCycle(source);
 		let neededCapacity = minEnergy - currentEstimate;
 
@@ -78,25 +80,124 @@ class Allocator implements Ticker {
 
 		// If we still need more capacity, spawn new creeps
 		neededCapacity -= capacityAdded;
-		const room = source.room as BaseRoom;
-		const harvesterBody = this.generateHarvesterBody(room, neededCapacity);
+		const harvesterBody = this.generateHarvesterBody(
+			source,
+			neededCapacity / ENERGY_REGEN_TIME,
+		);
 		const name = generateCreepName(ROLE_WORKER);
 		room.addCreepToSpawnQueue(harvesterBody, ROLE_WORKER, {});
 	}
 
-	public generateHarvesterBody(room: BaseRoom, maxCapacity: number) {
-		// Ratio of work and carry parts to move parts
-		// Higher travel time vs harvest time?
-		// harvest time will always be the same if the ratio of work to carry is the same
-		// and can be calculated as work * HARVEST_POWER / CARRY_CAPACITY if need be
-
-		// Guesstimate a good ratio for now
-		const targetHarvestDepositTimeRatio = 0.5;
+	/**
+	 * Generates a body for a harvester that can extract maxCapacity energy per tick on average from a source.
+	 * If the body is too large for MAX_CREEP_SIZE, or too expensive for the room's energy capacity, it will be capped.
+	 * @param source (Source) The source to generate the body for
+	 * @param maxCapacity (number) The maximum energy to extract from the source per tick
+	 * @returns (BodyPartConstant[]) The body for the harvester
+	 */
+	public generateHarvesterBody(source: Source, maxCapacity: number) {
+		// Guesstimate a good ratio for now; there is likely an optimal ratio
+		// for energy extracted per creep vs energy cost of said creep
+		// when considering the cost of (WORK + CARRY) vs (MOVE) parts
+		// but that's a problem for another day. This 1:1 ratio is "good enough" and easy to tweak by eye.
+		const targetHarvestDepositTimeRatio = 1;
 
 		// harvestTime is will always be the same assuming there's a 1:1 ratio of work to carry parts
 		const harvestTime = HARVEST_POWER / CARRY_CAPACITY;
 
-		// Calculate average
+		// (n / x) + b
+		const depositTimeToMoveRatioFcn = this.getMoveRatioFromTimeFunction(
+			source.id,
+		);
+
+		const targetDepositTime = harvestTime / targetHarvestDepositTimeRatio;
+		// Round to the nearest power of 2 (including negative powers)
+		// because you can't add less than 1 move part to a creep
+		const targetMoveRatio = depositTimeToMoveRatioFcn(targetDepositTime);
+		const roundedMoveRatio = roundToPowerOfTwo(targetMoveRatio);
+		// Cap move ratio at 2, as this will result in creeps with 1:1 work:move parts which will move at max speed
+		// (except on swamp. Might be worth considering in the future.)
+		const cappedMoveRatio = Math.min(roundedMoveRatio, 2);
+
+		// TODO: Use updated deposit time calculated from inverse depositTimeToMoveRatioFcn to account for rounding.
+		const calculatedDepositTime = targetDepositTime;
+		const segmentExtractionPerTick =
+			CARRY_CAPACITY / (harvestTime + calculatedDepositTime);
+
+		// Get the number of work segments needed to extract maxCapacity per tick
+		// maxCapacity / segmentExtractionPerTick * parts per segment ([WORK, CARRY])
+		const maxWorkSegmentsNeededForExtraction =
+			Math.ceil(maxCapacity / segmentExtractionPerTick) * 2;
+
+		// Get max possible work segments for a maximum size creep
+		// See notebook for math work again
+		const maxWorkSegmentsForSizeLimit = Math.floor(
+			MAX_CREEP_SIZE / (cappedMoveRatio + 2),
+		);
+
+		// Get max possible work segments for a maximum cost creep
+		// Same math as above but with energy cost instead of size cost
+		const costRatio =
+			BODYPART_COST[MOVE] / (BODYPART_COST[WORK] + BODYPART_COST[CARRY]);
+		const maxWorkSegmentsForCostLimit = Math.floor(
+			source.room.energyCapacityAvailable / (costRatio + 2),
+		);
+
+		// Take the minimum of the three limits as the number of work segments to use
+		const workSegments = Math.min(
+			maxWorkSegmentsNeededForExtraction,
+			maxWorkSegmentsForSizeLimit,
+			maxWorkSegmentsForCostLimit,
+		);
+
+		const moveParts = workSegments * cappedMoveRatio;
+
+		// Generate the body
+		const body: BodyPartConstant[] = [];
+		for (let i = 0; i < workSegments; i++) {
+			body.push(WORK, CARRY);
+		}
+		for (let i = 0; i < moveParts; i++) {
+			body.push(MOVE);
+		}
+
+		return body;
+	}
+
+	/**
+	 * Returns a curve that maps the average ratio of work to carry parts to the time taken to harvest and deposit energy
+	 * Uses the function ratio = (n / time) + offset
+	 * @param source (Id<Source>) The source to generate the curve for
+	 */
+	private getMoveRatioFromTimeFunction(sourceId: Id<Source>) {
+		const sourceDepositTimes = Array.from(
+			this.memory._sourceDepositTimes[sourceId],
+		);
+		sourceDepositTimes.sort((a, b) => a.workMoveRatio - b.workMoveRatio);
+
+		let meanOffset = 0;
+		let meanAmplitude = 0;
+
+		for (let i = 0; i < sourceDepositTimes.length - 1; i++) {
+			const time = sourceDepositTimes[i].time;
+			const nextTime = sourceDepositTimes[i + 1].time;
+			const ratio = sourceDepositTimes[i].workMoveRatio;
+			const nextRatio = sourceDepositTimes[i + 1].workMoveRatio;
+
+			// This is a linear approximation of the curve. With enough data points, this should be good enough
+			// See notebook for math behind these calculations
+			const offset =
+				(nextRatio * nextTime - ratio * time) / (nextRatio - ratio);
+			const amplitude = time * (ratio - offset);
+
+			meanOffset += offset;
+			meanAmplitude += amplitude;
+		}
+
+		meanOffset /= sourceDepositTimes.length - 1;
+		meanAmplitude /= sourceDepositTimes.length - 1;
+
+		return (depositTime: number) => meanAmplitude / depositTime + meanOffset;
 	}
 
 	/**
@@ -182,9 +283,9 @@ class Allocator implements Ticker {
 	 * @returns (number) The total extraction rate
 	 */
 	public calculateTotalExtractionRate(source: Source, creep: WorkerCreep) {
-		const workParts = creep.body.filter((part) => part.type === WORK).length;
+		const workSegments = creep.body.filter((part) => part.type === WORK).length;
 		const storageCapacity = creep.store.getCapacity();
-		const extractionRate = workParts * HARVEST_POWER;
+		const extractionRate = workSegments * HARVEST_POWER;
 		const extractionTime = storageCapacity / extractionRate;
 
 		return (
@@ -211,4 +312,17 @@ class Allocator implements Ticker {
 	}
 
 	public memory: AllocatorMemory = Memory.allocator;
+}
+
+/**
+ * Rounds a number to the nearest power of two
+ * @param num (number) The number to round
+ * @returns (number) The number rounded to the nearest power of two
+ */
+function roundToPowerOfTwo(num: number) {
+	const log = Math.log2(num);
+	const lower = 2 ** Math.floor(log);
+	const upper = 2 ** Math.ceil(log);
+	const mean = (lower + upper) / 2;
+	return num >= mean ? upper : lower;
 }
