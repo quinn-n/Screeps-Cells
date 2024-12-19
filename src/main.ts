568eb9f5446ff263cfb4c30df16e868fb71ec8ad
Quinn N <88954051+quinn-n@users.noreply.github.com>
2024-12-18 20:03:09 -0500

    Start ticking allocator, rooms and creeps in main

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,36 @@
+import Allocator from "./allocator";
 import cleanup from "./cleanup";
+import type { BaseCreep } from "./creep.base";
 import pixelGenerator from "./pixel.generator";
+import type { BaseRoom } from "./room";
 
 function loop() {
 	cleanup.run();
 
-	pixelGenerator.run();
+	tickAllocator();
+	tickRooms();
+	tickCreeps();
 
+	pixelGenerator.run();
+}
+
+function tickAllocator() {
+	const allocator = Allocator.Instance;
+
+	allocator.tick();
+}
+
+function tickRooms() {
+	for (const room of Object.values(Game.rooms) as BaseRoom[]) {
+		if (room.controller?.my) {
+			room.tick();
+		}
+	}
+}
+
+function tickCreeps() {
+	for (const creep of Object.values(Game.creeps) as BaseCreep[]) {
+		creep.tick();
 	}
 }
 
